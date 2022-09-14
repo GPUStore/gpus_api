edba9e186ec5a0aa7994346f2aa89878efd61ab3
alekseiiagnenkov <alekseiiagnenkov@gmail.com>
2022-09-14 11:14:15 +0300

    created

---
+++ src/main/java/ru/mephi/gpus_api/GpusApiApplication.java
@@ -0,0 +1,13 @@
+package ru.mephi.gpus_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpusApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpusApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/mephi/gpus_api/GpusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mephi.gpus_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpusApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
