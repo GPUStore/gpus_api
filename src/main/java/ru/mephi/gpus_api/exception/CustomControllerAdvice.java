17fe7385a0da80bb17f6fb374bb181645cf6b418
alekseiiagnenkov <alekseiiagnenkov@gmail.com>
2022-10-22 10:43:10 +0300

    added exceptions

---
+++ src/main/java/ru/mephi/gpus_api/exception/CustomControllerAdvice.java
@@ -0,0 +1,42 @@
+package ru.mephi.gpus_api.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.xml.bind.ValidationException;
+
+@Slf4j
+@RestControllerAdvice
+class CustomControllerAdvice {
+
+    @ExceptionHandler({GpuStoreException.class})
+    public ResponseEntity<String> handleGpuStoreExceptions(GpuStoreException e) {
+        log.error("Throws exception: ", e);
+        return new ResponseEntity<>(
+                e.getMessage(),
+                HttpStatus.valueOf(400)
+        );
+    }
+
+    @ExceptionHandler({ValidationException.class, HttpMessageNotReadableException.class})
+    public ResponseEntity<String> handleJsonExceptions(Exception e) {
+        log.error("Throws exception: ", e);
+        return new ResponseEntity<>(
+                ErrorMessage.VALIDATION_ERROR.toString(),
+                HttpStatus.valueOf(400)
+        );
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleOtherExceptions(Exception e) {
+        log.error("Throws exception: ", e);
+        return new ResponseEntity<>(
+                ErrorMessage.TECHNICAL_ERROR.toString(),
+                HttpStatus.valueOf(500)
+        );
+    }
+}
